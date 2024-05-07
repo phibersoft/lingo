ddd1e87e4262c331e3e2f11c144b872ba910822a
phibersoft <spirnet@hotmail.com>
2024-05-07 23:20:34 +0300

    fix: UserProgress uses inferred type from courses

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
 
 type UserProgressProps = {
-  activeCourse: { imageSrc: string; title: string }; // TODO
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
