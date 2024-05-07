eb6a74bc6349f6b34f468d565c1857656233b704
phibersoft <spirnet@hotmail.com>
2024-05-07 23:03:12 +0300

    feat: upsertUserProgress action implementation

---
+++ actions/user-progress.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { getCourseById, getUserProgress } from "@/db/queries";
+import db from "@/db/drizzle";
+import { userProgress } from "@/db/schema";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const upsertUserProgress = async (courseId: number) => {
+  const { userId } = auth();
+  const user = await currentUser();
+
+  if (!userId || !user) throw new Error("Unauthorized");
+
+  const course = await getCourseById(courseId);
+
+  if (!course) throw new Error("Course not found");
+
+  const existingUserProgress = await getUserProgress();
+
+  if (existingUserProgress) {
+    await db.update(userProgress).set({
+      activeCourseId: courseId,
+      userName: user.fullName || "Anonymous",
+      userImageSrc: user.imageUrl || "/mascot.svg",
+    });
+
+    revalidatePath("/courses");
+    revalidatePath("/learn");
+    redirect("/learn");
+  } else {
+    await db.insert(userProgress).values({
+      userId,
+      activeCourseId: courseId,
+      userName: user.fullName || "Anonymous",
+      userImageSrc: user.imageUrl || "/mascot.svg",
+    });
+  }
+};
