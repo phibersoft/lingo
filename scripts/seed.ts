334b5d7aaed04fb347263c6e098067bf6c94c612
phibersoft <spirnet@hotmail.com>
2024-05-08 00:05:55 +0300

    seed data for units, challenges, options

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -15,13 +15,79 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
+    console.log("Inserting courses...");
     await db.insert(schema.courses).values([
-      { title: "Spanish", imageSrc: "/es.svg" },
-      { title: "French", imageSrc: "/fr.svg" },
-      { title: "Italian", imageSrc: "/it.svg" },
-      { title: "Japanese", imageSrc: "/jp.svg" },
-      { title: "Croatian", imageSrc: "/hr.svg" },
+      { id: 1, title: "Spanish", imageSrc: "/es.svg" },
+      { id: 2, title: "French", imageSrc: "/fr.svg" },
+      { id: 3, title: "Italian", imageSrc: "/it.svg" },
+      { id: 4, title: "Japanese", imageSrc: "/jp.svg" },
+      { id: 5, title: "Croatian", imageSrc: "/hr.svg" },
+    ]);
+
+    console.log("Inserting units...");
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+    ]);
+
+    console.log("Inserting lessons...");
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        title: "Nouns",
+        order: 1,
+      },
+    ]);
+
+    console.log("Inserting challenges...");
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        question: 'Which one of these is the "the man"?',
+        order: 1,
+        type: "SELECT",
+      },
+    ]);
+
+    console.log("Inserting challenge options...");
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
     ]);
 
     console.log("Seeding finished.");
