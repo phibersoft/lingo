b65f90ec1a768f0f918de155f66191066deab8c0
phibersoft <spirnet@hotmail.com>
2024-05-07 23:20:09 +0300

    feat: seed script implementation

---
+++ scripts/seed.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql, {
+  schema,
+});
+
+const main = async () => {
+  try {
+    console.log("Seeding database...");
+
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+
+    await db.insert(schema.courses).values([
+      { title: "Spanish", imageSrc: "/es.svg" },
+      { title: "French", imageSrc: "/fr.svg" },
+      { title: "Italian", imageSrc: "/it.svg" },
+      { title: "Japanese", imageSrc: "/jp.svg" },
+      { title: "Croatian", imageSrc: "/hr.svg" },
+    ]);
+
+    console.log("Seeding finished.");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed database");
+  }
+};
+
+main();
