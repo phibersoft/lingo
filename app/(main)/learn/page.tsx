06cda37b2d55534ce2e39fb64bbbe693b8b73ca2
phibersoft <spirnet@hotmail.com>
2024-05-08 01:12:21 +0300

    feat: learn page calculates live data for courseProgress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,13 +3,28 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { Header } from "@/app/(main)/learn/header";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/db/queries";
 import { redirect } from "next/navigation";
+import { Unit } from "@/app/(main)/learn/unit";
 
 const LearnPage: NextPage = async () => {
   const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, courseProgress, lessonPercentage, units] =
+    await Promise.all([
+      userProgressData,
+      courseProgressData,
+      lessonPercentageData,
+      unitsData,
+    ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -27,7 +42,22 @@ const LearnPage: NextPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
-        <div className={"space-y-4"}></div>
+        {units.map((unit) => (
+          <div key={unit.id} className={"mb-10"}>
+            <Unit
+              {...unit}
+              activeLesson={
+                courseProgress?.activeLesson
+                  ? {
+                      ...courseProgress.activeLesson,
+                      unit,
+                    }
+                  : undefined
+              }
+              activeLessonPercentage={lessonPercentage}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
