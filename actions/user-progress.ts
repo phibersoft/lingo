2477d3490bd377c1d8ec8136f43e780d86dd42ec
phibersoft <spirnet@hotmail.com>
2024-05-09 19:22:54 +0300

    feat: reduceHearts action implementation

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -3,9 +3,10 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { getCourseById, getUserProgress } from "@/db/queries";
 import db from "@/db/drizzle";
-import { userProgress } from "@/db/schema";
+import { challengeProgress, challenges, userProgress } from "@/db/schema";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { and, eq } from "drizzle-orm";
 
 export const upsertUserProgress = async (courseId: number) => {
   const { userId } = auth();
@@ -37,4 +38,50 @@ export const upsertUserProgress = async (courseId: number) => {
   revalidatePath("/courses");
   revalidatePath("/learn");
   redirect("/learn");
+};
+
+export const reduceHearts = async (challengeId: number) => {
+  const { userId } = auth();
+
+  if (!userId) throw new Error("Unauthorized");
+
+  const currentUserProgress = await getUserProgress();
+  // TODO: Handle subscription
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  });
+
+  if (!challenge) throw new Error("Challenge not found");
+  const { lessonId } = challenge;
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, challengeId),
+    ),
+  });
+
+  const isPractice = !!existingChallengeProgress;
+
+  if (isPractice) return { error: "practice" };
+
+  if (!currentUserProgress) throw new Error("User progress not found");
+
+  // TODO: Handle subscription
+
+  if (currentUserProgress.hearts === 0) return { error: "hearts" };
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: currentUserProgress.hearts - 1,
+    })
+    .where(eq(userProgress.userId, userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/quests");
+  revalidatePath("/learn");
+  revalidatePath("/lesson");
+  revalidatePath(`/lesson/${lessonId}`);
 };
